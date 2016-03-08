cbdf8a626a3163de1f40340436b79f9d0a5ddb0d
NickStefan <nickstefan12@gmail.com>
2016-03-08 08:44:35 -0800

    server files from disk

--- simplewiki.go
+++ simplewiki.go
@@ -3,39 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	fmt.Println(filename, err, body)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main(){
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":7000", nil)
 }
-
-// type Page struct {
-// 	Title string
-// 	Body []byte
-// }
-
-// func (p *Page) save() error {
-// 	filename := p.Title + ".txt"
-// 	return ioutil.WriteFile(filename, p.Body, 0600)
-// }
-
-// func loadPage(title string) (*Page, error) {
-// 	filename := title + ".txt"
-// 	body, err := ioutil.ReadFile(filename)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &Page{Title: title, Body: body}, nil
-// }
-
-// func main() {
-// 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-// 	p1.save()
-// 	p2, _ := loadPage("TestPage")
-// 	fmt.Println(string(p2.Body))
-// }
